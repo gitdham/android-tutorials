1bfc0857de77291b555e2a198f31721525717619
gitdham <adzani234@gmail.com>
2021-04-28 13:47:09 +0700

    create custom toast

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_toast.*
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,12 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
     btnShowToast.setOnClickListener {
-      Toast.makeText(applicationContext, "Hi, I'm a toast", Toast.LENGTH_LONG).show()
+      //      Toast.makeText(applicationContext, "Hi, I'm a toast", Toast.LENGTH_LONG).show()
+      Toast(this).apply {
+        duration = Toast.LENGTH_LONG
+        view = layoutInflater.inflate(R.layout.custom_toast, clToast)
+        show()
+      }
     }
   }
 }
