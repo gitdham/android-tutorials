51cbdd675a2244a6cf462dc3c90e7ec9b127db01
gitdham <adzani234@gmail.com>
2021-04-28 14:36:06 +0700

    basic activity

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,22 +1,18 @@
 package com.example.tutorials
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.custom_toast.*
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    btnShowToast.setOnClickListener {
-      //      Toast.makeText(applicationContext, "Hi, I'm a toast", Toast.LENGTH_LONG).show()
-      Toast(this).apply {
-        duration = Toast.LENGTH_LONG
-        view = layoutInflater.inflate(R.layout.custom_toast, clToast)
-        show()
+    btnOpenActivity.setOnClickListener {
+      Intent(this, SecondActivity::class.java).also {
+        startActivity(it)
       }
     }
   }
---
+++ app/src/main/java/com/example/tutorials/SecondActivity.kt
@@ -0,0 +1,23 @@
+package com.example.tutorials
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_second)
+
+    btnNextActivity.setOnClickListener {
+      Intent(this, ThirdActivity::class.java).also{
+        startActivity(it)
+      }
+    }
+
+    btnBack.setOnClickListener {
+      finish()
+    }
+  }
+}
