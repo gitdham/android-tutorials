d42819ff0c0015ad945ec0506586d955a9010b5a
gitdham <adzani234@gmail.com>
2021-04-29 15:20:07 +0700

    simple toolbar menus

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,29 +1,50 @@
 package com.example.tutorials
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-
-    btnTakePhoto.setOnClickListener {
-      Intent(Intent.ACTION_GET_CONTENT).also {
-        it.type = "image/*"
-        startActivityForResult(it, 0)
-      }
-    }
   }
 
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-    if(resultCode == Activity.RESULT_OK && requestCode == 0){
-      val uri = data?.data
-      ivPhoto.setImageURI(uri)
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.app_bar_menu, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when(item.itemId) {
+      R.id.miAddContact -> Toast.makeText(
+        this,
+        "You clicked on add contact",
+        Toast.LENGTH_SHORT
+      ).show()
+
+      R.id.miFavorites  -> Toast.makeText(
+        this,
+        "You clicked on favorites",
+        Toast.LENGTH_SHORT
+      ).show()
+
+      R.id.miSettings   -> Toast.makeText(
+        this,
+        "You clicked on settings",
+        Toast.LENGTH_SHORT
+      ).show()
+
+      R.id.miFeedback   -> Toast.makeText(
+        this,
+        "You clicked on feedback",
+        Toast.LENGTH_SHORT
+      ).show()
+
+      R.id.miClose      -> finish()
     }
+    return true
   }
 }
