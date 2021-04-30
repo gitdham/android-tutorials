6f3156c84fd71f688f01f5c505cceffc6eded0ce
gitdham <adzani234@gmail.com>
2021-04-30 13:36:42 +0700

    alert dialog sample

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -4,12 +4,73 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val addContactDialog = AlertDialog.Builder(this)
+      .setTitle("Add contact")
+      .setMessage("Do you want to add Mr. X to your contact list?")
+      .setIcon(R.drawable.ic_add_contact_alert)
+      .setPositiveButton("Yes") { _, _ ->
+        Toast.makeText(this, "You added Mr. X to your contact", Toast.LENGTH_SHORT).show()
+      }
+      .setNegativeButton("No") { _, _ ->
+        Toast.makeText(this, "You didn't add Mr. X to your contact", Toast.LENGTH_SHORT)
+          .show()
+      }.create()
+
+    btnDialog1.setOnClickListener {
+      addContactDialog.show()
+    }
+
+    val options = arrayOf("First Item", "Second Item", "Third Item")
+    val singleChoiceDialog = AlertDialog.Builder(this)
+      .setTitle("Choose one of these options")
+      .setSingleChoiceItems(options, 0) { dialogInterface, i ->
+        Toast.makeText(this, "You clicked on ${options[i]}", Toast.LENGTH_SHORT).show()
+      }
+      .setPositiveButton("Accept") { _, _ ->
+        Toast.makeText(this, "You accepted the SingleChoiceDialog", Toast.LENGTH_SHORT)
+          .show()
+      }
+      .setNegativeButton("Decline") { _, _ ->
+        Toast.makeText(this, "You declined the SingleChoiceDialog", Toast.LENGTH_SHORT)
+          .show()
+      }.create()
+
+    btnDialog2.setOnClickListener {
+      singleChoiceDialog.show()
+    }
+
+    val multiChoiceDialog = AlertDialog.Builder(this)
+      .setTitle("Choose one of these options")
+      .setMultiChoiceItems(
+        options, booleanArrayOf(false, false, false)
+      ) { _, i, isChecked ->
+        if(isChecked) Toast.makeText(
+          this, "You checked ${options[i]}", Toast.LENGTH_SHORT
+        ).show()
+        else Toast.makeText(this, "You unchecked ${options[i]}", Toast.LENGTH_SHORT)
+          .show()
+      }
+      .setPositiveButton("Accept") { _, _ ->
+        Toast.makeText(this, "You accepted the MultiChoiceDialog", Toast.LENGTH_SHORT)
+          .show()
+      }
+      .setNegativeButton("Decline") { _, _ ->
+        Toast.makeText(this, "You declined the MultiChoiceDialog", Toast.LENGTH_SHORT)
+          .show()
+      }.create()
+
+    btnDialog3.setOnClickListener {
+      multiChoiceDialog.show()
+    }
   }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
